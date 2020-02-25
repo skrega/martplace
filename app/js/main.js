8d65e3248cb1d4623457d206c231ae7aa9d5386a
skrega <skorpionn440@gmail.com>
2020-02-25 21:16:31 +1000

    added section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
 $(function(){
+    $('.product__slider-items').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+    });
+     $('.slider__feed-inner').slick({
+         slidesToShow: 3,
+         slidesToScroll: 1,
+         prevArrow: '<button type="button" class="slick-prev"></button>',
+         nextArrow: '<button type="button" class="slick-next"></button>',
+     });
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+    
+
+
+
+    var mixer = mixitup('.product__inner-box');
 });
