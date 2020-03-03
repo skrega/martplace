5c4bf67d3c22998f4895727bd1aaa64d141c529f
skrega <skorpionn440@gmail.com>
2020-03-03 11:15:54 +1000

    added new-items

--- app/js/main.js
+++ app/js/main.js
@@ -18,8 +18,29 @@ $(function(){
         readOnly: true,
         ratedFill: "#ffc000"
     });
-    
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function () {
+        $('.product-page__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    });
+    $('.icon-th-large').on('click', function () {
+        $('.product-page__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+    });
+
     $('input, select').styler();
+
 
 
     var mixer = mixitup('.product__inner-box');
